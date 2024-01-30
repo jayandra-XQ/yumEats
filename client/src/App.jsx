e804da4f1317b192b3c5e8e1454de9547c0ab7cb
Jayandra Hamal <salonhamal278@gmail.com>
2024-01-30 22:19:23 +0530

    delivery part complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import Delivery from './components/Delivery'
 import Featured from './components/Featured'
 import TopNav from './components/TopNav'
 
@@ -6,6 +7,7 @@ const App = () => {
     <div className="App">
       <TopNav />
       <Featured />
+      <Delivery />
     </div>
   )
 }
